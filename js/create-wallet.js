03d8ccc74a9739f0bc9952518c5cf5843f4b3939
Caio Cesar Bosco <caio.c.bosco@gmail.com>
2021-08-25 13:53:36 -0300

    Using Webcrypto to Derive Password Key

--- js/create-wallet.js
+++ js/create-wallet.js
@@ -1,29 +1,3 @@
-const toPromise = (callback) => {
-    const promise = new Promise((resolve, reject) => {
-        try {
-            callback(resolve, reject);
-        }
-        catch (err) {
-            reject(err);
-        }
-    });
-    return promise;
-}
-
-const storageKey = () => {
-    const key = 'publicKeys';
-    const value = { name: '0x0123456789'};
-
-    const promise = toPromise((resolve, reject) => {
-        chrome.storage.local.set({ [key]: value }, () => {
-            if (chrome.runtime.lastError)
-                reject(chrome.runtime.lastError);
-
-            resolve(value);
-        });
-    });
-} 
-
 let submitButton = document.getElementById("create-wallet-submit-button");
 let message = document.getElementById("create-wallet-feedback-text");
 
@@ -42,11 +16,10 @@ function submitCreateWallet() {
         message.innerHTML = "Please insert a Valid Password"
     } else if (!confirm || confirm.lenght == 0) {
         message.innerHTML = "Please confirm the Password"
-    }else if (password != confirm) {
+    } else if (password != confirm) {
         message.innerHTML = "Confirmation does not match"
     } else {
         storageKey();
         window.location.href = 'secret-phrase.html';
     }
 }
-
--- js/keystore.js
+++ js/keystore.js
@@ -1,38 +1,69 @@
-async function storageKeyFileExist() {
+function storageKeyFileExist() {
 
-    var publicKeys = "";
-    publicKeys = await getPublicKeys();
-    if(!publicKeys || publicKeys.lenght == 0) {
+    test();
+    publicKeys = window.localStorage.getItem("keyStorage");
+
+    if(!publicKeys) {
         return false;
     } else {
         return true;
     }
 }
 
-function generateKeyPair() {
-    let (publicKey, privateKey) = nacl.box.keyPair;
+async function generateKeyPair() {
+
+    // Algorithm Object
+    var algorithmKeyGen = {
+        name: "RSASSA-PKCS1-v1_5",
+        modulusLength: 2048,
+        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),  // Equivalent to 65537
+        hash: {
+        name: "SHA-256"
+        }
+    };
+    
+    var algorithmSign = {
+        name: "RSASSA-PKCS1-v1_5"
+    };
+
+    var keyPair = await crypto.subtle.generateKey(algorithmKeyGen, true, ["sign"]);
+    return keyPair;
+
 }
 
-const toPromise = (callback) => {
-    const promise = new Promise((resolve, reject) => {
-        try {
-            callback(resolve, reject);
-        }
-        catch (err) {
-            reject(err);
-        }
-    });
-    return promise;
+async function deriveKey(password) {
+
+    let enc = new TextEncoder();
+    let passwordKey = await crypto.subtle.importKey("raw", enc.encode(password), "PBKDF2", false, ["deriveBits", "deriveKey"]);
+
+    var kdfParams = {
+        name: "PBKDF2",
+        salt: new Uint8Array(10),
+        iterations: 262144,
+        hash: "SHA-256"
+    };
+
+    var derivedAlgorithm = {name: 'aes-ctr', length: 128};
+    var derivedKey = await crypto.subtle.deriveKey(kdfParams, passwordKey, derivedAlgorithm, true, ['encrypt', 'decrypt']);
+    return derivedKey;
 }
 
-const getPublicKeys = () => {
-    return toPromise((resolve, reject) => {
-        chrome.storage.local.get(['publicKeys'], (result) => {
-            if (chrome.runtime.lastError)
-                reject(chrome.runtime.lastError);
+function encryptsPrivateKeyFile() {
 
-            const researches = result.publicKeys ?? [];
-            resolve(researches);
-        });
-    });
+    var data = asciiToUint8Array("hello");
+    var iv = new Uint8Array(16);    
+    var password = "1234";
+    var iv = new Uint8Array(16);
+    var derivedKey = deriveKey(password);
+    crypto.subtle.encrypt({name: 'AES-CTR', iv: iv}, derivedKey, data);
+
+
+}
+
+async function test() {
+
+    var derivedKey = await deriveKey("1234");
+    var expKey = await crypto.subtle.exportKey("jwk", derivedKey);
+    var stringfiedKey = JSON.stringify(expKey);
+    alert(stringfiedKey);
 }
