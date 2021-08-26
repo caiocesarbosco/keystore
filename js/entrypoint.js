6f31a0a5ec3a5bf6dade81b6a58869403e243431
Caio Cesar Bosco <caio.c.bosco@gmail.com>
2021-08-26 10:56:47 -0300

    Proposed Remo Homescreen UI

--- js/entrypoint.js
+++ js/entrypoint.js
@@ -1,9 +1,23 @@
-if(storageKeyFileExist()) {
-    window.location.href = 'user-auth.html';
+const userButton = document.getElementById("user-id");
+var user = window.localStorage.getItem("Users");
+userButton.value = "Use [" + user + "]";
+
+
+document.getElementById("create-account-submit").onclick = redirectCreateAccount;
+document.getElementById("import-account-submit").onclick = redirectImportAccount;
+document.getElementById("import-file-submit").onclick = redirectImportFile;
+userButton.onclick = redirectUserAuth;
+
+function redirectCreateAccount() {
+    window.location.href = 'create-wallet.html';
 }
 
-document.getElementById("create-wallet-submit").onclick = redirectCreateWallet;
+function redirectImportAccount() {
+}
 
-function redirectCreateWallet() {
-    window.location.href = 'create-wallet.html';
+function redirectImportFile() {
+}
+
+function redirectUserAuth() {
+    window.location.href = 'user-auth.html';
 }
